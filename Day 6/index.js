5aafd552d1e059a862d91343441683769a52c89c
Lisa <lis-zaa@live.com>
2023-03-08 14:52:29 -0600

    minor changes

--- Day 6/index.js
+++ Day 6/index.js
@@ -1,46 +1,46 @@
 const items = [
   {
-    name: "apple",
-    category: "fruit",
+    name: 'apple',
+    category: 'fruit',
   },
   {
-    name: "Cucumber",
-    category: "vegetable",
+    name: 'Cucumber',
+    category: 'vegetable',
   },
   {
-    name: "Banana",
-    category: "fruit",
+    name: 'Banana',
+    category: 'fruit',
   },
   {
-    name: "Celery",
-    category: "vegetable",
+    name: 'Celery',
+    category: 'vegetable',
   },
   {
-    name: "orange",
-    category: "fruit",
+    name: 'orange',
+    category: 'fruit',
   },
   {
-    name: "sausage",
-    category: "meat",
+    name: 'sausage',
+    category: 'meat',
   },
   {
-    name: "bacon",
-    category: "meat",
+    name: 'bacon',
+    category: 'meat',
   },
 ];
 
-const categorySelect = document.getElementById("category");
-const itemSelect = document.getElementById("item");
-const itemHeader = document.getElementById('item__header')
-const allCategories = new Set(items.map((item) => item.category));
-let currentCategory = "fruit";
+const categorySelect = document.getElementById('category');
+const itemSelect = document.getElementById('item');
+const itemHeader = document.getElementById('item__header');
+const allCategories = Array.from(new Set(items.map((item) => item.category)));
+let currentCategory = 'fruit';
 let categoryItems;
 let currentItem = itemSelect.value;
 
 function getItems(currentCategory) {
-   return categoryItems = items
-  .filter((item) => item.category === currentCategory)
-  .map((item) => item.name);
+  return (categoryItems = items
+    .filter((item) => item.category === currentCategory)
+    .map((item) => item.name));
 }
 
 function createOptions(list, container) {
@@ -50,27 +50,33 @@ function createOptions(list, container) {
     }
   }
 
-  list.forEach((element) => {
-    const option = document.createElement("option");
+  list.forEach((element, index) => {
+    const option = document.createElement('option');
+    if (index === 0) {
+      option.setAttribute('selected', '');
+    }
     option.textContent = element;
     option.value = element;
     container.appendChild(option);
   });
 }
 
-categorySelect.addEventListener("click", () => {
+categorySelect.addEventListener('change', () => {
   currentCategory = categorySelect.value;
-  getItems(currentCategory)
+  getItems(currentCategory);
   createOptions(categoryItems, itemSelect);
   itemHeader.textContent = itemSelect.value;
 });
 
-itemSelect.addEventListener("change", () => {
+itemSelect.addEventListener('change', () => {
   itemHeader.textContent = itemSelect.value;
 });
 
 // default options on first load
-createOptions(allCategories, categorySelect);
-getItems(currentCategory)
-createOptions(categoryItems, itemSelect);
-itemHeader.textContent = itemSelect.value;
+function initialize() {
+  createOptions(allCategories, categorySelect);
+  createOptions(getItems(currentCategory), itemSelect);
+  itemHeader.textContent = itemSelect.value;
+}
+
+initialize();
